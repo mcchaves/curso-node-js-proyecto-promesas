ae0d2dc30f84cc0cb8c792ce3d3eb2bfaea2f3e0
mcchaves <eleceya.2@gmail.com>
2018-10-16 19:22:40 +0200

    añadido getOpositor, getNotas, getResultado y getDatos

--- promises.js
+++ promises.js
@@ -4,7 +4,7 @@ const opositores = [{
   especialidad: 'Informática'
 }, {
   id: 2,
-  name: 'Leyre',
+  nombre: 'Leyre',
   especialidad: 'Sistemas y aplicaciones informáticas'
 }]
 
@@ -19,16 +19,85 @@ const notas = [{
 }, {
   id: 2,
   prueba: 'Práctica',
-  nota: 3.5
+  nota: 7.5
 }, {
   id: 2,
   prueba: 'Teórica',
-  nota: 6.5
+  nota: 8.5
 }
 ]
 
 // crea promesa para obtener los datos del opositor 1
+const getOpositor = (id) => {
+  return new Promise((resolve, reject) => {
+    const opositor = opositores.find((opositor) => opositor.id === id)
+    if (opositor) {
+      resolve(opositor)
+    } else {
+      reject(new Error(`No se ha encontrado al opositor con id: ${id}.`))
+    }
+  })
+}
+
+getOpositor(1)
+  .then(opositor => console.log(`opositor: ${opositor.nombre}`))
+  .catch(err => console.log(err))
+/*
+getOpositor(5)
+  .then(opositor => console.log(`opositor: ${opositor.name}`))
+  .catch(err => console.log(err))
+*/
 
 // crea promesa para obtener las notas del opositor 1
 
-// crea promesa para obtener el nombre y las notas del opositor1
+const getNotas = (id) => {
+  return new Promise((resolve, reject) => {
+    const notasOpositor = notas.filter((nota) => nota.id === id)
+    if (notasOpositor.length) {
+      resolve(notasOpositor)
+    } else {
+      reject(new Error(`No se ha encontrado notas del opositor con id: ${id}.`))
+    }
+  })
+}
+
+getNotas(1)
+  .then(notas => {
+    // console.log(notas)
+    notas.forEach(nota => {
+      console.log(`prueba ${nota.prueba}, nota: ${nota.nota}`)
+    })
+  }
+  )
+  .catch(err => console.log(err))
+
+// crea promesa para obtener el nombre y las notas del opositor 1
+const getResultado = (id) => {
+  let nombre
+  getOpositor(id)
+    .then((opositor) => {
+      nombre = opositor.nombre
+      return getNotas(id)
+    })
+    .then((notas) => {
+      const nota = notas.map((nota) => nota.nota).reduce((sum, x) => (sum + x) / notas.length)
+      console.log(`nota media: ${nota}`)
+    })
+}
+
+getResultado(1)
+
+const getDatos = (id) => {
+  Promise.all([getOpositor(id), getNotas(id)])
+    .then(datos => {
+      const { nombre } = datos[0]
+      const nota = datos[1].map((nota) => nota.nota).reduce((sum, x) => (sum + x) / datos[1].length)
+      console.log(`El opositor ${nombre} tiene una nota media de ${nota}`)
+      // console.log(nombre)
+      // console.log(nota)
+    })
+    .catch(err => console.log(err))
+}
+
+getDatos(2)
+// Pepe tiene una media de 5 en la oposición de Informática
--- sumaFiles.js
+++ sumaFiles.js
@@ -1,4 +1,48 @@
 const fs = require('fs')
+const Promise = require('bluebird')
+
+// lectura de datos de forma síncrona
+/*
 const numero1 = fs.readFileSync('./numero1', 'utf-8')
 const numero2 = fs.readFileSync('./numero2', 'utf-8')
-console.log(`El resultado de la suma es  ${parseInt(numero1)+parseInt(numero2)}`)
+*/
+
+// lectura de datos de forma asíncrona
+fs.readFile('./numero1', (err, data) => {
+  if (err) console.log(err)
+  const numero1 = parseInt(data)
+  // console.log(`el primer numero es ${numero1}`)
+
+  fs.readFile('./numero2', (err, data) => {
+    if (err) console.log(err)
+    const numero2 = parseInt(data)
+    // console.log(`el primer numero es ${numero2}`)
+    console.log(`El resultado de la suma de ${numero1} y ${numero2} es  ${numero1 + numero2}`)
+  })
+})
+
+// lectura de datos con promesas
+const getData = (fileName, type) => new Promise(
+  (resolve, reject) => {
+    fs.readFile(fileName, type, (err, data) => {
+      err ? reject(err) : resolve(parseInt(data))
+    })
+  }
+)
+
+const promise1 = getData('numero1', 'utf-8')
+const promise2 = getData('numero2', 'utf-8')
+Promise.all([promise1, promise2])
+  . then((arrayValues) => {
+    const sum = arrayValues.reduce((sum, x) => sum + x)
+    console.log(`Suma con promesas.  El resultado es ${sum}`)
+  })
+
+// usando fs-extra
+const promise11 = getData('numero1', 'utf-8')
+const promise12 = getData('numero2', 'utf-8')
+Promise.all([promise11, promise12])
+  . then((arrayValues) => {
+    const sum = arrayValues.reduce((sum, x) => parseInt(sum) + parseInt(x))
+    console.log(`Suma con promesas.  El resultado es ${sum}`)
+  })
