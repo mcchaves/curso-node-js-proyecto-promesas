4a65aa912131306ef298ddf12982c6a457ff89cc
mcchaves <eleceya.2@gmail.com>
2018-10-16 19:48:01 +0200

    creado datos.js para guardar los datos y promisesAsync para crear codigo asincrono

---
+++ datos.js
@@ -0,0 +1,33 @@
+const opositores = [{
+  id: 1,
+  nombre: 'Pepe',
+  especialidad: 'Informática'
+}, {
+  id: 2,
+  nombre: 'Leyre',
+  especialidad: 'Sistemas y aplicaciones informáticas'
+}]
+
+const notas = [{
+  id: 1,
+  prueba: 'Práctica',
+  nota: 3.5
+}, {
+  id: 1,
+  prueba: 'Teórica',
+  nota: 6.5
+}, {
+  id: 2,
+  prueba: 'Práctica',
+  nota: 7.5
+}, {
+  id: 2,
+  prueba: 'Teórica',
+  nota: 8.5
+}
+]
+
+module.exports = {
+  opositores,
+  notas
+}
--- promisesAsync.js
+++ promisesAsync.js
@@ -1,7 +1,7 @@
 const { opositores, notas } = require('./datos.js')
 
 // crea promesa para obtener los datos del opositor 1
-const getOpositor = (id) => {
+const getOpositores = (id) => {
   return new Promise((resolve, reject) => {
     const opositor = opositores.find((opositor) => opositor.id === id)
     if (opositor) {
@@ -12,31 +12,40 @@ const getOpositor = (id) => {
   })
 }
 
+// crea promesa para obtener los datos del opositor 1
+const getOpositor = async (id) => {
+  const opositor = await opositores.find((opositor) => opositor.id === id)
+  if (opositor) {
+    return opositor
+  } else {
+    throw (new Error(`No se ha encontrado el opositor con id: ${id}.`))
+  }
+}
+
 getOpositor(1)
   .then(opositor => console.log(`opositor: ${opositor.nombre}`))
   .catch(err => console.log(err))
 
 // crea promesa para obtener las notas del opositor 1
-const getNotas = (id) => {
-  return new Promise((resolve, reject) => {
-    const notasOpositor = notas.filter((nota) => nota.id === id)
-    if (notasOpositor.length) {
-      resolve(notasOpositor)
-    } else {
-      reject(new Error(`No se ha encontrado notas del opositor con id: ${id}.`))
-    }
-  })
+const getNotas = async (id) => {
+  const notasOpositor = await notas.filter((nota) => nota.id === id)
+  if (notasOpositor.length) {
+    return notasOpositor
+  } else {
+    throw (new Error(`No se ha encontrado notas del opositor con id: ${id}.`))
+  }
 }
 
 getNotas(1)
   .then(notas => {
-    // console.log(notas)
     notas.forEach(nota => {
       console.log(`prueba ${nota.prueba}, nota: ${nota.nota}`)
     })
   }
   )
   .catch(err => console.log(err))
+
+/*
 
 // crea promesa para obtener el nombre y las notas del opositor 1
 const getResultado = (id) => {
@@ -67,3 +76,4 @@ const getDatos = (id) => {
 }
 
 getDatos(2)
+*/
